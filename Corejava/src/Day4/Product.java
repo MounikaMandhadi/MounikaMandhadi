dd38821d3adc4cb312323743a42fe1366ebf1dae
mandha03 <146457500+mandha03@users.noreply.github.com>
2024-01-14 21:00:17 -0600

    Updated Product.java

--- Corejava/src/Day4/Product.java
+++ Corejava/src/Day4/Product.java
@@ -1,5 +1,21 @@
 package Day4;
 
+
 public class Product {
 
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Overloaded getPrice method that takes quantity as a parameter
+    public double getPrice(int quantity) {
+        // Calculate the total price based on the quantity
+        return price * quantity;
+    }
 }
