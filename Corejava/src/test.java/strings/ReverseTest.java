611bce7d36e960a2c594dd59bd33fdb4bb58a585
MounikaMandhadi <mounikaredddy@0619@gmail.com>
2024-01-23 10:09:09 -0600

    J-unit test cases

---
+++ Corejava/src/test.java/strings/ReverseTest.java
@@ -0,0 +1,16 @@
+package strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class ReverseTest {
+
+	@Test
+	void test() {
+		boolean exp = true;
+        Assert.assertEquals(true,true);
+	}
+
+}
---
+++ Corejava/src/test.java/strings/VowelsTest.java
@@ -0,0 +1,16 @@
+package strings;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class VowelsTest {
+
+	@Test
+	void test() {
+		boolean exp = true;
+        Assert.assertEquals(true,true);
+	}
+
+}
+
+//cmd+shift+o+a
